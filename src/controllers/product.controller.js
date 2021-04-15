27e65643556d66e0637122f8d38f2f96b8048263
anibaljose <anibal.rodriguez.sistemas@gmail.com>
2021-04-14 20:21:56 -0600

    i create the method getProducts, getProductByID and createProduct. I saved the json to send request from postman -> In this moment i practice english, i will hope to get bettern

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,21 +1,29 @@
 import { json } from "express"
 import Product from '../models/Product'
 
-export const getProducts = (req, res) => {
-    res.json('get products');    
+export const getProducts = async (req, res) => {
+   const products = await Product.find(); 
+   res.json(products);
 }
 
-export const createProduct = (req, res) => {
-    console.log(req.body);
-    res.json('creating product')
+export const createProduct = async (req, res) => {
+    const {name, category, price, imgUrl} = req.body
+
+    const newProduct = new Product({name, category, price, imgUrl});
+    //i add "await" and "async""" in the name of the main function, to wait for the function to finish
+    const productSaved = await newProduct.save();
+    res.status(201).json(productSaved)
 }
 
-export const getProductById = (req, res) => {
+export const getProductById = async (req, res) => {
+    const product = await Product.findById(req.params.productId);
+    res.status(200).json(product)
+}
+
+export const updateProductById = async (req, res) => {
     
 }
-export const updateProductById = (req, res) => {
-    
-}
-export const deleteProductById = (req, res) => {
+
+export const deleteProductById = async(req, res) => {
     
 }
