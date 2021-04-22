052224c48926407c80c0c965ac8f9900e2ed0b18
anibaljose <anibal.rodriguez.sistemas@gmail.com>
2021-04-21 19:50:54 -0600

    Se agrego validacion para roles de usuarios

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -1,5 +1,6 @@
-import jwt from 'jsonwebtoken'
+import jwt, { decode } from 'jsonwebtoken'
 import config from '../config'
+import Role from '../models/Role';
 import User from '../models/User'
 
  export const verifyToken  = async(req, res, next) => {
@@ -7,23 +8,36 @@ import User from '../models/User'
          
         const token = req.headers["x-access-token"]
 
-        console.log(token)
+        //console.log(token)
 
         //comprobamos si se envio el token
         if(!token) return res.status(403).json({message: "No token provider"})
 
         //extraer lo que esta en el toke
         const decoded = jwt.verify(token, config.SECRET)
+        req.userId = decoded.id;
 
         //buscamos al usuario de la extraccion de jwt
-        const user = await User.findById(decoded.id , {password:0 })
+        const user = await User.findById(req.userId , {password:0 })
 
         if(!user) return res.status(404).json({message: 'no user found'})
 
-        console.log(user)
+        //console.log(user)
         //para continuar con la peticion y siga su curso
         next()
      }catch(error){
         return res.status(401).json({message: 'Not authorized'})
      }
+};
+
+export const isModerator = async (req, res, next) => {
+   //como en el anterior metodo verifytoken tengo el req.userID, puedo obtenerlo aqui tambien
+   const user = await User.findById(req.userId)
+   const roles = await Role.find({_id: {$in: user.roles}})
+   console.log(roles)
+   next()
+}
+
+export const isAdmin = async (req, res, next) => {
+   
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -8,22 +8,24 @@ const router = Router()
 import * as productCtrl from '../controllers/product.controller'
 
 //exportar el middleware donde contiene la verificacion del token
-import {verifyToken} from '../middlewares'
+import {authJwt} from '../middlewares'
 
 //con solo indicarle el slash / y en app.js llamar app.use('/products', productRoute) me indicara que antes del slash va precedido
 // con products
 //router.get('/', (req, res) => res.json('get products'))
 
 //cambiamos lo que esta arriba con el controlador que creamos en cada ruta
+//en la rutas podemos agregar middlewere para comprobar antes de que ingesar al ruta, como el token, si es admin, moderador
+//o cualquier otra cosa que queremos validar
 
-router.get('/', verifyToken, productCtrl.getProducts)
+router.get('/', [authJwt.verifyToken, authJwt.isModerator], productCtrl.getProducts)
 
-router.post('/', verifyToken, productCtrl.createProduct)
+router.post('/', [authJwt.verifyToken], productCtrl.createProduct)
 
-router.get('/:productId', verifyToken, productCtrl.getProductById)
+router.get('/:productId', [authJwt.verifyToken], productCtrl.getProductById)
 
-router.put('/:productId', verifyToken, productCtrl.updateProductById)
+router.put('/:productId', [authJwt.verifyToken], productCtrl.updateProductById)
 
-router.delete('/:productId', verifyToken, productCtrl.deleteProductById)
+router.delete('/:productId', [authJwt.verifyToken, authJwt.isModerator], productCtrl.deleteProductById)
 
 export default router;
